5e36fef0e5a95f1215bbee844aea80ecbafe25ad
kseniazakharkina <63050158+kseniazakharkina@users.noreply.github.com>
2020-10-30 20:08:26 +0300

    -

--- check.php
+++ check.php
@@ -1,9 +1,9 @@
 <?php
-$login = filter_var(trim($_POST['login']),
+$login = filter_var (trim($_POST['login']),
 FILTER_SANITIZE_STRING);
-$name = filter_var(trim($_POST['name']),
+$name = filter_var (trim($_POST['name']),
 FILTER_SANITIZE_STRING);
-$pass = filter_var(trim($_POST['pass']),
+$pass = filter_var (trim($_POST['pass'],
 FILTER_SANITIZE_STRING);
 
 if(mb_strlen($login))< 5 || mb _strlen($login)>90)
