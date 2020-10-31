8e0a53dcd9b8ba1f47cd97c5fc6e2396942bd4e7
kseniazakharkina <63050158+kseniazakharkina@users.noreply.github.com>
2020-10-31 14:32:00 +0300

    переделываем

---
+++ php-auth/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<Html Lang="en">
+<head>
+    <meta charset="UTF-8" >
+    <title>Авторизация и регистрация</title>
+<link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+<h1>Форма авторизации</h1>
+<form action="" method=" ">
+    <label>Логин</label>
+    <input type="text">
+    <label>Пароль</label>
+    <input type="password">
+    <button>Войти</button>
+    
+
+</form>
+
+</body>
+</html>
